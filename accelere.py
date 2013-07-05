099ec97096c35f04914ec43bd124ddb272c1ec17
maome <steelereilly@gmail.com>
2013-07-05 10:01:45 -0700

    added more comments

--- accelere.py
+++ accelere.py
@@ -9,13 +9,16 @@ import signal
 import time
 import Queue
 
-
+#Override the error def on argparse.ArgumentParser
+#to display specific information on error
 class CleanErrorParser(argparse.ArgumentParser):
     def error(self, message):
         sys.stderr.write('error: %s\n' % message)
         sys.stderr.write('use -h or --help for usage information\n')
         #self.print_help()
         sys.exit(2)
+
+#Create, configure, and run the parser
 parser = CleanErrorParser()
 parser.add_argument("-m", "--max", 	        type=int, 	help="the maximum number of images to keep",        default=1024)
 parser.add_argument("-i", "--interval",     type=float, help="the interval between threads",                default=60.0)
@@ -27,27 +30,31 @@ parser.add_argument("--dev", 				            help="the program runs in developme
 parser.add_argument("--dry", 				            help="does not acquire images, only shows debug",   action="store_true")
 args = parser.parse_args()
 
+#error handling for no arguments (accelere.py is 1 argument)
 if len(sys.argv)==1: #if we have no arguments:
     parser.print_help()
     sys.exit()
 
-
-
-
-
+#bool interrupted becomes true if sigint is received
 interrupted = False
-done = False
-def signal_handler(signum, frame):
+
+#handle sigints by setting interrupted to true
+def signal_interrupt_handler(signum, frame):
     global interrupted
     print "\rInterrupted, waiting for wayward threads.."
     interrupted = True
-signal.signal(signal.SIGINT, signal_handler)
 
-count = 0
+#define handler function for sigints to be signal_interrupt_handler
+signal.signal(signal.SIGINT, signal_interrupt_handler)
+
+#Tracks the number of images download attempts
+image_download_count = 0
 
 # http://images.wsdot.wa.gov/nw/525vc00820.jpg
 # http://140.160.161.198/axis-cgi/jpg/image.cgi?camera=1&resolution=460x345&compression=0
 
+#TODO: comment this better? clean this up?
+#Checks if dev is true and sets required variables
 if args.dev == True:
     max_images = 28800
     thread_interval = 3.0
@@ -61,7 +68,8 @@ else:
     image_url = args.location
     storage_dir = args.directory
 
-#check storage directory
+#check storage directory and attempts to create if it does not exist
+#exits if there is an error creating the directory
 if not os.path.isdir(storage_dir):
     print "Directory " + storage_dir + " does not exist, attempting to create..."
     try:
@@ -88,15 +96,20 @@ try:
     settings_file.close()
 except:
     print "Error saving location to settings file (does someone else have a lock?)"
+
+#Prints variable output before accelere starts
 print   "\nAccelere started\n" + \
         "  Image URL:\t"      + image_url + "\n" + \
         "  Max Images:\t"     + str(max_images) + "\n" + \
         "  Interval:\t"       + str(thread_interval) + "s\n" + \
         "  Storage Dir\t" + storage_dir + "\n"
+
 # Queue to store our timestamps so we know how large to let the window get
 # before we start deleting old images.
 q = Queue.Queue(max_images)
 
+#Creates the downloader thread using the downloadImage function
+#Starts the thread and returns a reference to it
 def spawnDownloaderThread():
     thread = threading.Thread(target=downloadImage)
     #thread.daemon = True
@@ -106,15 +119,15 @@ def spawnDownloaderThread():
 # Grab an image from the desired URL and store it appropriately. Then manage
 # the queue as necessary.
 def downloadImage():
-    global count
+    global image_download_count
     timestamp = str(int(time.time()))
-    count = count + 1
-    print "Retrieving image " + str(count) + "..."
+    image_download_count = image_download_count + 1
+    print "Retrieving image " + str(image_download_count) + "..."
 
     # Download the image and save it.
     if args.dry != True:
     	urllib.urlretrieve(image_url, storage_dir + timestamp + image_type);
-        print "Downloaded image " + str(count) + " to " + storage_dir + timestamp + image_type   
+        print "Downloaded image " + str(image_download_count) + " to " + storage_dir + timestamp + image_type   
  
     # Check whether the queue is full. If so, remove the oldest timestamp.
     if q.full():
@@ -124,12 +137,16 @@ def downloadImage():
     q.put(timestamp)
 
 # Run the program
-
-# Cleanly exit by pressing q. Will wait on pending images.
+# Cleanly exit by pressing ctrl-c. Will wait on pending images.
 
 while not interrupted:
     thread = spawnDownloaderThread() #this call will block if sigint has told us we are terminating
     if not interrupted: #I don't know how to make this part cleaner, we want to bail before the sleep
         time.sleep(thread_interval)
+
+#wait on last thread (this could potentially cut off image downloads that remain unfinished
+#if they began before the most recent thread, could use a pool to handle this I think? but not
+#sure if that should matter because the resource would not even be available if a new image has
+#been stored (is this even how webservers work if a download is in progress and the resource changes?)
 thread.join()
-print "done."
+print "Done."
